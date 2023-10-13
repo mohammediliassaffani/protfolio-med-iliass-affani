d85c678d208e7b2c1aa957962031135db99fac87
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-10-13 23:51:57 +0100

    set-up-client

--- PORTFOLIO-iliass/src/container/About/About.jsx
+++ PORTFOLIO-iliass/src/container/About/About.jsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants'
+
 import './About.scss';
+
+const abouts = [
+  {
+    title: 'Web Development',
+    description: 'I am a good devlopper',
+    imgUrl: images.about01,
+  },
+  {
+    title: 'Mobile Development',
+    description: 'I am a smart devlopper',
+    imgUrl: images.about02,
+  },
+  { title: 'Web UI/UX', 
+  description: 'I am a creative devlopper', 
+  imgUrl: images.about03 
+},
+  { title: 'Web backend', 
+  description: 'I am a good devlopper', 
+  imgUrl: images.about04 
+}
+];
 
 const About = () => {
   return (
-    <div>
-      <h2>I Know</h2>
-    </div>
+    <>
+      <h2 className="head-text">
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 };
 
