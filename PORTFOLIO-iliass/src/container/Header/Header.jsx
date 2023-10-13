874a7e19bb0101648453bf8bbc71e9f845752c8e
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-10-13 22:53:37 +0100

    header-end

--- PORTFOLIO-iliass/src/container/Header/Header.jsx
+++ PORTFOLIO-iliass/src/container/Header/Header.jsx
@@ -3,9 +3,22 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id='home' className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -19,6 +32,10 @@ const Header = () => {
               <h1>Mohammed Iliass</h1>
             </div>
           </div>
+          <div className="tag-cmp app__flex">
+          <p className="p-text">Web & Mobile Developer</p>
+          <p className="p-text">Freelancer</p>
+        </div>
         </div>
       </motion.div>
       <motion.div
@@ -34,6 +51,17 @@ const Header = () => {
         alt="profile_circle"
         className="overlay_circle"
       />
+    </motion.div>
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.vue, images.react, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
     </motion.div>
     </div>
   );
