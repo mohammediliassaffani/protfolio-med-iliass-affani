7f168b722f6e3b9dd4d1876bd317033a356ae3bc
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-10-13 21:25:26 +0100

    header-setup

--- PORTFOLIO-iliass/src/container/Header/Header.jsx
+++ PORTFOLIO-iliass/src/container/Header/Header.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 import './Header.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <div className="p-text">Hello I am</div>
+              <h1>Mohammed Iliass</h1>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className="app__header-img"
+    >
+      <img src={images.profile} alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.circle}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
+    </div>
+  );
 };
 
 export default Header;
