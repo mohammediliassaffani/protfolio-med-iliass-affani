0dfed820370398c012763308d82c1dc848414cf9
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-10-15 14:19:21 +0100

    add-abouts-from-sanity

--- PORTFOLIO-iliass/src/container/About/About.jsx
+++ PORTFOLIO-iliass/src/container/About/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Design</span> <br />
+        I Know that <span>Good Apps</span> <br />
         means <span>Good Business</span>
       </h2>
 
