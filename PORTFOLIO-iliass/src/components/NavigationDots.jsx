71763f420598bed0ea9b1cc5649bf05c7c80e42b
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-10-16 20:04:35 +0100

    up-date-dote

--- PORTFOLIO-iliass/src/components/NavigationDots.jsx
+++ PORTFOLIO-iliass/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
+      {['home', 'about', 'work', 'skills', 'contact'].map(
         (item, index) => (
           <a 
           className='app__navigation-dot'
